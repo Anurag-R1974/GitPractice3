c7218add3d46c2b6b69ae9a99cf73082c7acb4f1
Anurag-R1974 <sanuragramagiri@gmail.com>
2025-04-10 02:13:20 +0530

    changed USKEY1 TO USKEY2 BY THE Windows dev

--- LixWin.py
+++ LixWin.py
@@ -5,7 +5,7 @@ client = boto3.client('ec2', region_name='us-east-1')  # You can change region a
 response = client.run_instances(
     ImageId='ami-00a929b66ed6e0de6',
     InstanceType='t2.micro',
-    KeyName='USKEY1',
+    KeyName='USKEY2', # Changed USKEY1 TO USKEY2 by the windows Dev
     MinCount=1,
     MaxCount=1
 )
